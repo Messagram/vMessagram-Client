36865d805c0fa639e4603e901f35baa2ccb4a42a
0xLuLz <87556649+0xLulz@users.noreply.github.com>
2022-03-12 14:11:32 -0800

    Create main.v

---
+++ src/messagram/main.v
@@ -0,0 +1,91 @@
+/*
+	Messagram.v Chat Connection
+*/
+module messagram
+
+import io
+import os
+import net
+
+pub struct Messagram {
+	pub mut:
+		buffer		string
+}
+
+const (
+	host = "skrillec.ovh"
+	port = 30
+)
+
+pub fn (mut m Messagram) messagram_connect() {
+	mut server := net.dial_tcp("${host}:${port}") or {
+		println("[x] Error, Unable to connect to messagram server!")
+		exit(0)
+	}
+
+	m.listener(mut server)
+}
+
+pub fn (mut m Messagram) listener(mut server net.TcpConn) {
+	mut reader := io.new_buffered_reader(reader: server)
+	for {
+		data := reader.read_line() or { "" }
+
+		if data.len == 0 || data == "" { continue }
+		// Validate JSON Response
+
+		cmd := ""
+
+		// Checking for 'action' parameters in the JSON string
+		if validate_key_in_json(data, "action") {
+			cmd = get_key_value(data, "action").replace(",")
+		}
+
+		if cmd == "msg" {
+			msg := get_key_value(data, "content")
+			m.buffer = msg
+		}
+
+	}
+}
+
+pub fn (mut m Messagram) check_new_msg() bool {
+	if m.buffer != "" {
+		return true
+	}
+	return false
+}
+
+pub fn (mut m Messagram) grab_new_msg() string {
+	if m.buffer != "" {
+		resp := m.buffer
+		m.buffer = ""
+		return resp
+	}
+	return ""
+}
+ 
+/*
+		Some custom JSON Functions
+*/
+
+pub fn validate_key_in_json(j string, key string) bool {
+	json_line := j.split("\n")
+	for i, line in json_line {
+		if line.contains("\"${key}\":") {
+			return true
+		}
+	}
+	return false
+}
+
+pub fn get_key_value(j string, key string) string {	
+	json_line := j.split("\n")
+	for i, line in json_line {
+		if line.contains("\"${key}\":") {
+			mut fixing := line.split(":")[1]
+			return fixing.replace("\"", "")
+		}
+	}
+	return ""
+}
