ed5337c37ed0ef3dec3dea98c948c1a6bdc0f0b6
0xLuLz <87556649+0xLulz@users.noreply.github.com>
2022-03-12 15:06:22 -0800

    Update main.v

--- main.v
+++ main.v
@@ -1,14 +1,21 @@
+import time
 import src.messagram // src/messagram/main.v
 
 fn main() {
 	mut m := messagram.Messagram{}
-	go buffer_listener(m)
+	go messagram.messagram_connect(mut &m)
+	buffer_listener(mut &m)
+	time.sleep(2*time.second)
+	m.send_msg("test\n")
+	for {
+
+	}
 }
 
 fn buffer_listener(mut m messagram.Messagram) {
 	for {
 		if m.check_new_msg() == true {
-			msg = m.grab_new_msg()
+			msg := m.grab_new_msg()
 			// place your text anywhere. im just gonnah print it normally
 			print(msg)
 		}
