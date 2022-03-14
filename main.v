daea19cf5035a292d80cb109d9dd4db67492f072
Erupt <101347500+Eruptsy@users.noreply.github.com>
2022-03-14 00:12:14 -0400

    Update main.v

--- main.v
+++ main.v
@@ -5,18 +5,15 @@ import src.messagram // src/messagram/main.v
 fn main() {
 	mut m := messagram.Messagram{}
 	go messagram.messagram_connect(mut &m)
-	go buffer_listener(mut &m)
-	time.sleep(2*time.second)
-	m.send_msg('{"status": "true/false","cmd": "msg","userid": "username_here","client_name": ""}\n')
-	for {
-
-	}
+	on_message(mut &m)
 }
 
-fn buffer_listener(mut m messagram.Messagram) {
+fn on_message(mut m messagram.Messagram) {
+	time.sleep(1*time.second)
+	m.send_msg('{"status": "true/false","cmd": "msg","userid": "username_here","client_name": ""}\n')
 	for {
-		if m.check_new_msg() == true {
-			msg := m.grab_new_msg()
+		if m.check_new_msg() == true { // if we recieved a new message
+			msg := m.grab_new_msg() // grab new message
 			// place your text anywhere. im just gonnah print it normally
 			print(msg)
 		}
